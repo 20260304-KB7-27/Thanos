68a1544d60796e8c4beea4d1825f1405dd0c66ea
Kimd0ng <kdongh0406@gmail.com>
2026-04-06 10:31:05 +0900

    feat: rout link 설정

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,13 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  routes: [
+    {
+      path: '/student',
+      component: () => import('@/pages/DonghyeonPage.vue'),
+    },
+  ],
+});
 
-export default router
+export default router;
